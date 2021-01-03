203f598890ae1282d1e34acc85c0d6ec902a82bb
SeoSeunghun <proto_seo@naver.com>
2021-01-03 19:44:56 +0900

    [프로그래머스] 콜라츠 추측

---
+++ Programmers/콜라츠 추측.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    if(num == 1) return 0;
+    for(int i=1;i<=500;i++){
+        if(num%2==0){
+            num /= 2;
+        }else if(num%2==1){
+            num = (num * 3) + 1;
+        }
+        if(num == 1) return i;
+    }
+    return -1;
+}
