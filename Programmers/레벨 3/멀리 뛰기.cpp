6ed085a531a58a34a7a8e3d813f82c27fd8e792a
SeoSeunghun <proto_seo@naver.com>
2021-02-01 14:32:30 +0900

    [프로그래머스] 멀리 뛰기

---
+++ Programmers/레벨 3/멀리 뛰기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    long long cache[2001]={0,1,2};
+    for(int i=3;i<=n;i++){
+        cache[i] = (cache[i-1] + cache[i-2])%1234567;
+    }
+    return cache[n];
+}
